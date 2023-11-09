3d08043a2362f05277cd96e359f6a9f0fb36f8b8
Yujin-nKim <yujinkimvv@gmail.com>
2023-11-09 20:58:08 +0900

    GameRepository 코드 구현

---
+++ src/Class/GameRepository.java
@@ -0,0 +1,11 @@
+package Class;
+
+import java.util.List;
+
+public class GameRepository {
+    List<Question> questionsCapital; // 수도문제 리스트
+    List<Question> questionsConnection; // 이어말하기 문제 리스트
+    List<Question> questionsFourLetters; // 사자성어 문제 리스트
+
+    
+}
