1453bb53daefd9da3a1927e98a7974420773b9c3
95x8x9 <sh2020200@naver.com>
2023-11-09 21:02:11 +0900

    변수 선언

---
+++ src/GUI/Placehold.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import javax.swing.*;
+
+class GUI extends JFrame {
+	private JPanel StartPanel, CategoryPanel, QPanel, EndPanel, RankingPanel, MarkingPanel;
+	private JTextField Nametxt, Qtxt, Answertxt;
+	private JButton Nextb1, Nextb2, Nextb3, Backb1, Backb2, ToRankingb, ToFirstb, ToMarkingb;
+	
+	
+	
+}
