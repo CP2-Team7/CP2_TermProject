637c389ad57bff1c7c477d19355fbfb20148b289
Yujin-nKim <yujinkimvv@gmail.com>
2023-11-13 03:25:14 +0900

    Question setting 함수 작성

--- src/Class/GameRepository.java
+++ src/Class/GameRepository.java
@@ -10,29 +10,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameRepository {
+    private final String[] questionNameList = {"capital", "connection", "fourletters"};
+    private final String[] questionList = {"questionsCapital", "questionsConnection", "questionsFourLetters"};
     List<Question> questionsCapital; // 수도문제 리스트
     List<Question> questionsConnection; // 이어말하기 문제 리스트
     List<Question> questionsFourLetters; // 사자성어 문제 리스트
-    List<Question> questionList;
 
-    public boolean setQuestion(String questionName) throws IOException {
+    public boolean setQuestion() throws IOException {
+        System.out.println(questionNameList.toString() + "게임 문제 세팅을 시작합니다.");
+        for (String questionName : questionNameList) {
+            setQuestionList(questionName);
+            System.out.println(questionName + "문제 세팅을 완료했습니다.");
+        }
+        return true;
+    }
+
+    private void setQuestionList(String questionName) {
         String path = System.getProperty("user.dir") + "/question/" + questionName + ".csv";
         BufferedReader br;
-        questionList = new ArrayList<>();
 
         try {
             br = new BufferedReader(new FileReader(path));
             String line = "";
+            br.readLine();
             while((line = br.readLine()) != null) {
                 String[] arr = line.split(",");
-                Question question = new Question("questionName", arr[0], arr[1]);
-                questionList.add(question);
+                Question question = new Question(questionName, arr[0], arr[1]);
+                if(questionName.equals("capital")){
+                    questionsCapital.add(question);
+                }else if(questionName.equals("connection")) {
+                    questionsConnection.add(question);
+                }else if(questionName.equals("fourletters")) {
+                    questionsFourLetters.add(question);
+                }
             }
             br.close();
         }catch (IOException e) {
             System.out.println(e.getMessage());
-            return false;
         }
-        return true;
     }
+
 }
