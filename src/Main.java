831194dbd1439f63c715d04bfbaf6a2291060d87
Yujin Kim <67141385+Yujin-nKim@users.noreply.github.com>
2023-11-13 19:31:34 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("JunSu");
         System.out.println("Hello world!");
-        System.out.println("test");
-
-        System.out.println("湲덉냼�쁽");
-
-        System.out.println("KSJ");
-        System.out.println("源��쑀吏�");
-
     }
 }
