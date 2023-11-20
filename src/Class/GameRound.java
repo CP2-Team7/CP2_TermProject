fd960ab650f94e6aa1063e553b6ea3840cac0b1e
Yujin-nKim <yujinkimvv@gmail.com>
2023-11-20 14:27:06 +0900

    GameRound 정답 비교 메소드 작성

--- src/Class/GameRound.java
+++ src/Class/GameRound.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class GameRound {
     final int stepNumber = 10;
+    final int scoreRate = 10;
     QuestionName currentCategory;
     User currentUser;
     List<Question> questionList;
@@ -22,20 +23,38 @@ public class GameRound {
 
     private void setQuestionListRandom(QuestionName currentCategory) {
         List<Question> list = new ArrayList<>();
-        if(currentCategory.equals(QuestionName.CAPITAL)) {
+        if (currentCategory.equals(QuestionName.CAPITAL)) {
             list = GameRepository.questionsCapital;
-        }else if(currentCategory.equals(QuestionName.CONNECTION)) {
+        } else if (currentCategory.equals(QuestionName.CONNECTION)) {
             list = GameRepository.questionsConnection;
-        }else if(currentCategory.equals(QuestionName.FOURLETTERS)) {
+        } else if (currentCategory.equals(QuestionName.FOURLETTERS)) {
             list = GameRepository.questionsFourLetters;
         }
 
         Integer[] arr = setRandomNumber(stepNumber, list.size());
 
         // 앞서 추출한 랜덤 넘버로 문제를 뽑고 출제할 문제 리스트에 추가
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             questionList.add(list.get(arr[i]));
+            answerList.add(list.get(arr[i]).answer);
         }
+    }
+
+    // 유저가 제출한 답과 문제의 정답을 비교해 채점한 후 score 기록
+    // 유저가 제출한 답 : String 타입의 list (문제 순서대로)
+    // return : total score (int)
+    public int checkAnswer(List<String> userAnswerList) {
+        int totalScore = 0;
+
+        for(int i = 0; i < answerList.size(); i++) {
+            if(answerList.get(i).equals(userAnswerList.get(i))) {
+                scoreList.add(1);
+                totalScore++;
+            } else {
+                scoreList.add(0);
+            }
+        }
+        return totalScore * scoreRate;
     }
 
     // 중복 없이 범위 내에서 난수 생성
